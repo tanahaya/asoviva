60c674e6a32ca4428bb8af097a1bd0e2378f8ade
tanahaya <tanahaya00623@gmail.com>
2016-11-08 20:57:27 +0900

    searchViewcon

---
+++ asoviva/searchViewController.swift
@@ -0,0 +1,35 @@
+//
+//  searchViewController.swift
+//  asoviva
+//
+//  Created by 田中千洋 on 2016/11/08.
+//  Copyright © 2016年 田中 颯. All rights reserved.
+//
+
+import UIKit
+
+class searchViewController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
