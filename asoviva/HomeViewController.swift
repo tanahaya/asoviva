c34a9220b907a0fcc91e9f6748a7477a98fefecc
tanahaya <tanahaya00623@gmail.com>
2017-01-24 20:52:16 +0900

    改行直した

--- asoviva/HomeViewController.swift
+++ asoviva/HomeViewController.swift
@@ -14,7 +14,6 @@ import ObjectMapper
 
 class HomeViewController: UIViewController, MKMapViewDelegate, UISearchBarDelegate,UITableViewDelegate,UITableViewDataSource,UISearchControllerDelegate{
     
-    
     var mapView: MKMapView = MKMapView()
     var Region: MKCoordinateRegion!
     var lat: CLLocationDegrees!
@@ -27,13 +26,10 @@ class HomeViewController: UIViewController, MKMapViewDelegate, UISearchBarDelega
     var searchController = UISearchController(searchResultsController: nil)
     var searchBar:UISearchBar!
     var key = "AIzaSyDJlAPjHOf0UirK-NomfpAlwY6U71soaNY"
-    
-    
-    override func viewDidLoad() {
+        override func viewDidLoad() {
         super.viewDidLoad()
         
         self.view.backgroundColor = UIColor.white
-        
         self.navigationController?.title = "Asoviva"
         
         locationManager = CLLocationManager()
@@ -52,7 +48,7 @@ class HomeViewController: UIViewController, MKMapViewDelegate, UISearchBarDelega
         lat  = 35.680298// 35.681298
         // lng = locationManager.location!.coordinate.longitude
         lng = 139.766247
-        
+            
         let mapframe: CGRect = CGRect(x: 0, y: 60 + 30, width: self.view.frame.width, height: self.view.frame.height*4/7)
         mapView.frame = mapframe
         let myLatitude: CLLocationDegrees = lat
@@ -82,7 +78,6 @@ class HomeViewController: UIViewController, MKMapViewDelegate, UISearchBarDelega
         self.view.addSubview(searchBar)
     }
     
-    
     func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
         switch status {
         case .notDetermined:
@@ -93,143 +88,79 @@ class HomeViewController: UIViewController, MKMapViewDelegate, UISearchBarDelega
             break
         }
     }
+    
     func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
         
         storenames = []
         searchBar.endEditing(true)
-        
         let page_token:String = ""
         let semaphore = DispatchSemaphore(value: 0)
-        /*
-         repeat {
-         //セマフォを使って、検索とメインスレッドを同期で処理する。
-         let semaphore = DispatchSemaphore(value: 0)
-         _ = key.addingPercentEncoding(withAllowedCharacters: NSCharacterSet.urlQueryAllowed)
-         let encodeStr = searchBar.text!
-         let a_encodeStr = encodeStr.addingPercentEncoding(withAllowedCharacters: NSCharacterSet.urlQueryAllowed)
-         _ = page_token.addingPercentEncoding(withAllowedCharacters: NSCharacterSet.urlQueryAllowed)
-         
-         let url = "https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=\(lat!),\(lng!)&radius=2000&sensor=true&key=\(key)&name=\(a_encodeStr!)"
-         //&pagetoken=\(page_token)なし
-         print(url)
-         let testURL:URL = URL(string: url)!
-         let session = URLSession(configuration: URLSessionConfiguration.default)
-         session.dataTask(with: testURL, completionHandler: { (data : Data?, response : URLResponse?, error : Error?) in
-         if error != nil {
-         print("\(error)")
-         } else {
-         if let statusCode = response as? HTTPURLResponse {
-         if statusCode.statusCode != 200 {
-         print("\(response)")
-         }
-         }
-         do {
-         let json = try JSONSerialization.jsonObject(with: data!, options:  JSONSerialization.ReadingOptions.mutableContainers) as! NSDictionary
-         let results = json["results"] as? Array<NSDictionary>
-         
-         for result in results! {
-         let annotation = MKPointAnnotation()
-         
-         let location: Location = Mapper<Location>().map(JSONObject: result)!
-         // self.locations.append(location)
-         self.storenames.append((result["name"] as? String)!)
-         // self.annotationList.append((result["locaiton"] as? MKPointAnnotation)! )
-         
-         if let geometry = result["geometry"] as? NSDictionary {
-         if let location = geometry["location"] as? NSDictionary {
-         
-         //ビンの座標を設定する。
-         annotation.coordinate = CLLocationCoordinate2DMake(location["lat"] as! CLLocationDegrees, location["lng"] as! CLLocationDegrees)
-         self.annotationList.append(annotation)
-         
-         }
-         }
-         }
-         
-         } catch {
-         print("エラー")
-         }
-         }
-         sleep(1)
-         //処理終了をセマフォに知らせる。
-         semaphore.signal()
-         }).resume()
-         //検索が終わるのを待つ。
-         _ = semaphore.wait(timeout: DispatchTime.distantFuture)
-         } while (page_token != "")
-         //ピンをマップに追加する。
-         */
-            let encodeStr = searchBar.text!
-            let a_encodeStr = encodeStr.addingPercentEncoding(withAllowedCharacters: NSCharacterSet.urlQueryAllowed)
-            let url = "https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=\(lat!),\(lng!)&radius=2000&sensor=true&key=\(key)&name=\(a_encodeStr!)"
-            let testURL:URL = URL(string: url)!
-            let session = URLSession(configuration: URLSessionConfiguration.default)
-            session.dataTask(with: testURL, completionHandler: { (data : Data?, response : URLResponse?, error : Error?) in
-                if error != nil {
-                    print("\(error)")
-                } else {
-                    if let statusCode = response as? HTTPURLResponse {
-                        if statusCode.statusCode != 200 {
-                            print("\(response)")
-                        }
-                    }
-                    do {
-                        let json = try JSONSerialization.jsonObject(with: data!, options:  JSONSerialization.ReadingOptions.mutableContainers) as! NSDictionary
-                        let results = json["results"] as? Array<NSDictionary>
-                        for result in results! {
-                            let annotation = MKPointAnnotation()
-                            let location: Location = Mapper<Location>().map(JSONObject: result)!
-                            // self.locations.append(location)
-                            self.storenames.append((result["name"] as? String)!)
-                            // self.annotationList.append((result["locaiton"] as? MKPointAnnotation)! )
-                            if let geometry = result["geometry"] as? NSDictionary {
-                                if let location = geometry["location"] as? NSDictionary {
-                                    //ビンの座標を設定する。
-                                    annotation.coordinate = CLLocationCoordinate2DMake(location["lat"] as! CLLocationDegrees, location["lng"] as! CLLocationDegrees)
-                                    self.annotationList.append(annotation)
-                                }
-                            }
-                        }
-                        
-                    } catch {
-                        print("エラー")
+        let encodeStr = searchBar.text!
+        let a_encodeStr = encodeStr.addingPercentEncoding(withAllowedCharacters: NSCharacterSet.urlQueryAllowed)
+        let url = "https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=\(lat!),\(lng!)&radius=2000&sensor=true&key=\(key)&name=\(a_encodeStr!)"
+        let testURL:URL = URL(string: url)!
+        
+        let session = URLSession(configuration: URLSessionConfiguration.default)
+        session.dataTask(with: testURL, completionHandler: { (data : Data?, response : URLResponse?, error : Error?) in
+            if error != nil {
+                print("\(error)")
+            } else {
+                if let statusCode = response as? HTTPURLResponse {
+                    if statusCode.statusCode != 200 {
+                        print("\(response)")
                     }
                 }
-                sleep(1)
-                //処理終了をセマフォに知らせる。
-                semaphore.signal()
-            }).resume()
-            //検索が終わるのを待つ。
-            _ = semaphore.wait(timeout: DispatchTime.distantFuture)
-            storeTableView.reloadData()
-        
-        //  mapView.addAnnotations(locations[].location as! [MKAnnotation])
+                do {
+                    let json = try JSONSerialization.jsonObject(with: data!, options:  JSONSerialization.ReadingOptions.mutableContainers) as! NSDictionary
+                    let results = json["results"] as? Array<NSDictionary>
+                    for result in results! {
+                        let annotation = MKPointAnnotation()
+                        let location: Location = Mapper<Location>().map(JSONObject: result)!
+                        // self.locations.append(location)
+                        self.storenames.append((result["name"] as? String)!)
+                        // self.annotationList.append((result["locaiton"] as? MKPointAnnotation)! )
+                        if let geometry = result["geometry"] as? NSDictionary {
+                            if let location = geometry["location"] as? NSDictionary {
+                                //ビンの座標を設定する。
+                                annotation.coordinate = CLLocationCoordinate2DMake(location["lat"] as! CLLocationDegrees, location["lng"] as! CLLocationDegrees)
+                                self.annotationList.append(annotation)
+                            }
+                        }
+                    }
+                } catch {
+                    print("エラー")
+                }
+            }
+            sleep(1)
+            //処理終了をセマフォに知らせる。
+            semaphore.signal()
+        }).resume()
+        //検索が終わるのを待つ。
+        _ = semaphore.wait(timeout: DispatchTime.distantFuture)
+        storeTableView.reloadData()
         //mapView.addAnnotation(annotationList as! MKAnnotation)
-        
-        
     }
+    
     func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
     }
     
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         mapView.setCenter(self.annotationList[indexPath.row].coordinate, animated: false)
         mapView.selectAnnotation(self.annotationList[indexPath.row], animated: true)
-        
         locationManager.startUpdatingLocation()
         
-        
     }
+    
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return storenames.count
     }
+    
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "MyCell", for: indexPath as IndexPath)
         //cell.textLabel?.text = locations[indexPath.row].storename
         cell.textLabel?.text = storenames[indexPath.row]
         return cell
-        
     }
-    
     
 }
 
